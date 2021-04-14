8d2e372e7fc631abb59f7e808c40919817b992f3
remi <remi.longin@outlook.fr>
2021-04-15 00:15:35 +0200

    Change logo

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,17 +1,6 @@
 <template>
   <v-app id="nav">
     <v-app-bar color="gray" style="flex: 0" flat>
-      <v-img
-        class="mx-2"
-        src="../assets/garmin_logo.png"
-        max-height="100"
-        max-width="100"
-        href="contain"
-      ></v-img>
-      <v-avatar
-        :color="$vuetify.breakpoint.smAndDown ? 'black' : 'transparent'"
-        size="32"
-      ></v-avatar>
       <v-tabs centered class=" mt-4; tabs" color="black" v-model="activeTab">
         <v-tab
           class="tab"
